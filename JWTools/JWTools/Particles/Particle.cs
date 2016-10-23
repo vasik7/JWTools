a791dbed5040362d8fdc68eba7698219f89558a2
vasik7 <vasik7@hotmail.com>
2016-10-23 22:29:27 +0200

    Particles

---
+++ JWTools/JWTools/Particles/Particle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWTools.Particles
+{
+   public class Particle
+    {
+        public string Name;
+    }
+}
